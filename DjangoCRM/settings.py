be2fcfbe1ea8d62e593e48cc830cf53a6910c804
Linas Jurevicius <linas.jurevicius@yahoo.com>
2023-10-23 23:39:13 +0300

    1

--- DjangoCRM/settings.py
+++ DjangoCRM/settings.py
@@ -134,7 +134,7 @@ CHANNEL_LAYERS = {
         #'BACKEND': 'channels.layers.InMemoryChannelLayer',
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         "CONFIG": {
-           "hosts": [os.environ.get('127.0.0.1', 6379)],
+           "hosts": [(os.environ.get('REDIS_HOST', 'localhost'),17639)]
         },
     }
 }
