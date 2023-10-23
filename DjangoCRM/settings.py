cd70dccc53d2ff135a38954867ef72162921a9ab
Linas Jurevicius <linas.jurevicius@yahoo.com>
2023-10-23 19:59:54 +0300

    s

--- DjangoCRM/settings.py
+++ DjangoCRM/settings.py
@@ -130,11 +130,11 @@ ASGI_APPLICATION = 'DjangoCRM.asgi.application'
 
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels.layers.InMemoryChannelLayer',
-        #'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        #"CONFIG": {
-         #   "hosts": [os.environ.get('127.0.0.1', 6379)],
-        #},
+        #'BACKEND': 'channels.layers.InMemoryChannelLayer',
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        "CONFIG": {
+           "hosts": [os.environ.get('127.0.0.1', 6379)],
+        },
     }
 }
 
--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 release: python manage.py migrate
-web: gunicorn DjangoCRM.asgi:application -k uvicorn.workers.UvicornWorker
+web: daphne config.asgi:application --port $PORT --bind 0.0.0.0 -v2
+worker: python manage.py runworker channels --settings=core.settings -v2
