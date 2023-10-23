fa04b71cd1aa5384288f13377c35f1482d3870f2
Linas Jurevicius <linas.jurevicius@yahoo.com>
2023-10-24 01:04:21 +0300

    some

--- DjangoCRM/asgi.py
+++ DjangoCRM/asgi.py
@@ -12,13 +12,13 @@ import django
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.routing import get_default_application
 
-os.environ.setdefault(
-    'DJANGO_SETTINGS_MODULE',
-    'DjangoCRM.settings')
-django_asgi_app = get_asgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','DjangoCRM.settings')
+django.setup()
+#django_asgi_app = get_asgi_application()
+django_asgi_app = get_default_application()
 
-#django.setup()
 
 from chat import routing
 
--- DjangoCRM/settings.py
+++ DjangoCRM/settings.py
@@ -134,7 +134,18 @@ CHANNEL_LAYERS = {
         #'BACKEND': 'channels.layers.InMemoryChannelLayer',
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         "CONFIG": {
-           "hosts": [(os.environ.get('REDIS_HOST', 'localhost'),6379)]
+          "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+        },
+    }
+}
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'channels_redis.cache.RedisCache',
+        'LOCATION': [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         },
     }
 }
